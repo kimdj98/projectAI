1a0f622834590bd85d12a44406b1f5aeb3b0d5f0
kimdj <kimdj98g@gmail.com>
2022-11-30 19:39:15 +0900

    implement 2d extraction

--- problem1-b.py
+++ problem1-b.py
@@ -10,6 +10,15 @@ from torch import optim
 import torch.nn.functional as F
 
 #==================================================================================================================
+#   set gpu device
+#==================================================================================================================
+USE_CUDA = torch.cuda.is_available()
+print(USE_CUDA)
+
+device = torch.device("cuda:0" if USE_CUDA else "cpu")
+print(f"device: {torch.cuda.get_device_name(device) }")
+
+#==================================================================================================================
 #   use configuration file to adjust hyper parameters
 #==================================================================================================================
 import json
@@ -32,9 +41,11 @@ from torchvision.datasets import MNIST
 # download path 정의
 download_root = './MNIST_DATASET'
 
-train_dataset = MNIST(download_root, transform=mnist_transform, train=True, download=True)
-valid_dataset = MNIST(download_root, transform=mnist_transform, train=False, download=True)
-test_dataset = MNIST(download_root, transform=mnist_transform, train=False, download=True)
+train_dataset = MNIST(download_root, transform=mnist_transform, train=True, download=False)
+valid_dataset = MNIST(download_root, transform=mnist_transform, train=False, download=False)
+test_dataset = MNIST(download_root, transform=mnist_transform, train=False, download=False)
+
+
 
 # option 값 정의
 batch_size = 64
@@ -181,15 +192,14 @@ X_train = X_train.float()
 y_train = torch.from_numpy(y_train)
 y_train = y_train.long()
 
-network = CNN(configs)
 
 # function to evaluate network
 def evaluation(network, dataloader):
     count = 0
     accuracy = 0.0
     for data in dataloader:
-        X_test = data[0]
-        y_test = data[1]
+        X_test = data[0].to(device)
+        y_test = data[1].to(device)
         y_hat = torch.argmax(network.forward(X_test), axis = 1)
         accuracy += torch.sum((y_test == y_hat).float()) # sum all the matched samples
 
@@ -197,14 +207,21 @@ def evaluation(network, dataloader):
 
     return accuracy
 
-evaluation(network, train_loader)
 
 
 #==================================================================================================================
 #   Training Stage
 #==================================================================================================================
+
+network = CNN(configs).to(device)
+evaluation(network, train_loader)
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(network.parameters(), configs["learning_rate"])
+
+loss_history_per_batch = []
+train_accuracy_per_epoch = []
+test_accuracy_per_epoch = []
 print("Start Training")
 for epoch in range(configs["epochs"]):  # loop over the dataset multiple times
 
@@ -212,7 +229,8 @@ for epoch in range(configs["epochs"]):  # loop over the dataset multiple times
     for i, data in enumerate(train_loader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-
+        inputs = inputs.to(device)
+        labels = labels.to(device)
         # zero the parameter gradients
         optimizer.zero_grad()
 
@@ -224,8 +242,30 @@ for epoch in range(configs["epochs"]):  # loop over the dataset multiple times
         optimizer.step()
 
         # print statistics
-        running_loss = loss.item()
-        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss:.3f}')
-    print(f'accuracy: {evaluation(network, train_loader)}')
+        if (i+1) % 100 == 0:
+            running_loss = loss.item()
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss/100:.6f}')
+            running_loss = 0.0
+            loss_history_per_batch.append(running_loss)
+    train_accuracy = evaluation(network, train_loader)
+    test_accuracy = evaluation(network, test_loader)
+
+    print(f'train accuracy: {train_accuracy}')
+    print(f'test accuracy:  {test_accuracy}')
+
+    train_accuracy_per_epoch.append(train_accuracy)
+    test_accuracy_per_epoch.append(test_accuracy)
 
 print('Finished Training')
+
+for i, data in enumerate(train_loader, 0):
+    inputs, labels = data
+    inputs = inputs.to(device)
+    labels = labels.to(device)
+    output_2d = network.extract_2d(inputs)
+    if i == 0:
+        output_2d_trains = output_2d.cpu().detach().numpy()
+    else:
+        output_2d_trains = np.vstack((output_2d_trains, output_2d.cpu().detach().numpy()))
+
+labels_2d_trains = train_dataset.train_labels.cpu().detach().numpy()

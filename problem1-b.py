4bab9ba762a7a1d120621f3ff51a81fc78087905
kimdj <kimdj98g@gmail.com>
2022-12-04 00:46:50 +0900

    finish 2d extraction

--- problem1-b.py
+++ problem1-b.py
@@ -262,32 +262,22 @@ for i, data in enumerate(train_loader, 0):
     inputs, labels = data
     inputs = inputs.to(device)
     labels = labels
-    output_2d = network.extract_2d(inputs)
-    if i == 0:
-        output_2d_trains = output_2d.cpu().detach().numpy()
-        output_2d_labels = labels.cpu().detach().numpy()
-    if i < 50:
-        output_2d_trains = np.vstack((output_2d_trains, output_2d.cpu().detach().numpy()))
-        output_2d_labels = np.concatenate((output_2d_labels, labels))
-
-print(output_2d_trains.shape)
-print(output_2d_labels.shape)
-plt.scatter(output_2d_trains[:,0], output_2d_trains[:,1], c=output_2d_labels)
+    output_2d = network.extract_2d(inputs).cpu().detach().numpy()
+    output_2d_labels = labels.cpu().detach().numpy()
+    plt.scatter(output_2d[:, 0], output_2d[:, 1], c=output_2d_labels)
+plt.title("Train data visualization")
+plt.xlabel("Activation of the 1st neuron")
+plt.ylabel("Activation of the 2nd neuron")
 plt.show()
 
-for i, data in enumerate(test_loader, 0):
+for i, data in enumerate(train_loader, 0):
     inputs, labels = data
     inputs = inputs.to(device)
     labels = labels
-    output_2d = network.extract_2d(inputs)
-    if i == 0:
-        output_2d_trains = output_2d.cpu().detach().numpy()
-        output_2d_labels = labels.cpu().detach().numpy()
-    if i < 50:
-        output_2d_trains = np.vstack((output_2d_trains, output_2d.cpu().detach().numpy()))
-        output_2d_labels = np.concatenate((output_2d_labels, labels))
-
-print(output_2d_trains.shape)
-print(output_2d_labels.shape)
-plt.scatter(output_2d_trains[:,0], output_2d_trains[:,1], c=output_2d_labels)
+    output_2d = network.extract_2d(inputs).cpu().detach().numpy()
+    output_2d_labels = labels.cpu().detach().numpy()
+    plt.scatter(output_2d[:, 0], output_2d[:, 1], c=output_2d_labels)
+plt.title("Test data visualization")
+plt.xlabel("Activation of the 1st neuron")
+plt.ylabel("Activation of the 2nd neuron")
 plt.show()
